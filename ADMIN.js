707d73e0933fab8533faa0929e108e31eb6f5cb1
Henda88 <ouni_henda@live.fr>
2018-09-27 11:16:50 +0100

    new flight page

--- ADMIN.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("lol").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("lol").style.marginLeft = "0";
-} 
